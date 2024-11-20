12661d91aa53334799578bb601a09ae8006565b7
KOSMO <nuremirik@gmail.com>
2024-11-20 23:09:21 +0600

    my changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Css from './Styles/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={Css.App}>
+      <div className={Css.NavBar}> 
+
+      </div>
+      <div className={Css.Content}>
+
+      </div>
+      <div className={Css.FooterBar}>
+        
+      </div>
     </div>
   );
 }
